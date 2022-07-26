73f1e0dfce452dd795955671fe495e86278955a4
Janis <janis.alves1@gmail.com>
2022-07-26 14:31:26 -0300

    Instalação e iniciação do react

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return(
+      <div>
+      <h1 className='titulo'>Hello!</h1>
+      <p className='texto'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati aspernatur ex minus quam sequi impedit velit, 
+        quia dolore ipsam dolores enim sint architecto inventore, omnis labore pariatur eum consectetur temporibus.</p>
+      </div>
+    )
+}
+
+export default Home;

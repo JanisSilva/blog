ab16d573ab8da83e457daa6ffd2191fca2124c42
Janis <janis.alves1@gmail.com>
2022-07-27 14:02:27 -0300

    Criação de NavBar, Home e Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import NavBar from './components/estaticos/navbar/NavBar';
+import Footer from './components/estaticos/footer/Footer';
 import Home from './paginas/home/Home';
+import './App.css';
 
 
 function App() {
  return (
-  <Home />
+  <>
+  <NavBar/>
+  <Home/>
+  <Footer/>
+ </>
  )
 }
 
